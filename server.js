7c484a2d2b92e6c5b6eb82e24a809a9c61e7c992
unknown <danny.driess@gmx.de>
2015-02-11 18:40:00 +0100

    Header components

--- server.js
+++ server.js
@@ -20,9 +20,8 @@ expressServer.set('view engine', 'ejs');
 
 expressServer.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
 
+var data = {headerObj : [{name : "Wohnzimmer", buttons : [{name:"Licht 1",topic:'light/licht1',type:"1"},{name:"Nils",topic:'light/licht1',type:"1"},{name:"Licht 2",topic:"light/licht2",type:"1"},{name:"Licht 2",topic:"light/licht2",type:"1"},{name:"Licht 2",topic:"light/licht2",type:"1"},{name:"Licht 2",topic:"light/licht2",type:"1"}]},{name : "Esszimmer", buttons : [{name:"Licht 3",topic:'light/licht3',type:"1"},{name:"Licht 4",topic:"light/licht4",type:"1"}]}]};
 
-//var data = {headerObj : [{name : "Wohnzimmer"}, {name: "Esszimmer"}], buttons : [{name:"Licht 1",topic:'light/licht1',type:"1"},{name:"Licht 2",topic:"light/licht2",type:"1"},{name:"ZettelZ",topic:"light/zettelz",type:"1"}]};
-var data = {headerObj : [{name : "Wohnzimmer", buttons : [{name:"Licht 1",topic:'light/licht1',type:"1"},{name:"Licht 2",topic:"light/licht2",type:"1"},{name:"Licht 2",topic:"light/licht2",type:"1"},{name:"Licht 2",topic:"light/licht2",type:"1"},{name:"Licht 2",topic:"light/licht2",type:"1"}]},{name : "Esszimmer", buttons : [{name:"Licht 3",topic:'light/licht3',type:"1"},{name:"Licht 4",topic:"light/licht4",type:"1"}]}]};
 //index routing
 expressServer.get('/', function(req, res) {
     res.render('index',data);
